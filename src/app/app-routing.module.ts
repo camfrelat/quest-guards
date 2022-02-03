4fca1083b25499765e8126210cb081c1ebe1c099
camille <camfrelat@gmail.com>
2022-02-03 14:13:47 +0100

    created service and guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { IsAdminGuard } from './core/is-admin.guard';
+import { IsConnectedGuard } from './core/is-connected.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'homepage' },
   { path: 'homepage', component: HomepageComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'admin', component: AdminComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [IsAdminGuard, IsConnectedGuard],
+  },
+  { path: 'admin', component: AdminComponent, canActivate: [IsAdminGuard] },
 ];
 
 @NgModule({
